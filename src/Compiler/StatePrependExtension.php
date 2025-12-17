0634e7cb080e79d02dd5f026f32d45b608539787
tacman <tacman@users.noreply.github.com>
2025-12-17 19:32:39 +0000

    babel improvements

--- src/Compiler/StatePrependExtension.php
+++ src/Compiler/StatePrependExtension.php
@@ -23,7 +23,7 @@ final class StatePrependExtension
         $raw = $builder->getExtensionConfig($alias);
         $queuePrefix       = '';
         $workflowPaths     = [$builder->getParameter('kernel.project_dir') . '/src/Workflow'];
-        $asyncTransportDsn = 'doctrine://default';
+        $asyncTransportDsn = 'doctrine://messenger';
 
         foreach (array_reverse($raw) as $cfg) {
             if (isset($cfg['queue_prefix']))        { $queuePrefix       = (string) $cfg['queue_prefix']; }
